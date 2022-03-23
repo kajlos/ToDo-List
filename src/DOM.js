8289e13604f0b1d7880491a2f062df304fefa2f3
Kajetan <kajetan.loska@op.pl>
2022-03-23 16:00:05 +0100

    .

--- src/DOM.js
+++ src/DOM.js
@@ -2,22 +2,34 @@ import Project from "./projects";
 const UI = (() => {
     const createProjectButton = document.getElementById('addProjectButton');
     const projectsList = document.getElementById('projects');
-    const createForm=()=>{
+    // const createForm=()=>{
+    //   const li = document.createElement('li');
+    //   const form = document.createElement('form');
+    //   const input = document.createElement('input');
+    //   input.type='text';
+    //   input.placeholder='Name';
+    //   const confirmButton = document.createElement('button');
+    //   const cancelButton = document.createElement('button');
+    //   form.append(input, confirmButton, cancelButton);
+    //   li.append(form);
+    //   projectsList.append(li);
+    // }
+    createProjectButton.addEventListener('click',()=>{
+      const li = document.createElement('li');
       const form = document.createElement('form');
       const input = document.createElement('input');
       input.type='text';
       input.placeholder='Name';
       const confirmButton = document.createElement('button');
+      confirmButton.textContent="create";
+      confirmButton.classList.add('confirm');
       const cancelButton = document.createElement('button');
-      return form.append(input, confirmButton, cancelButton);
-    }
-    createProjectButton.addEventListener('click',()=>{
-      const li = document.createElement('li');
-      li.append(createForm());
+      cancelButton.textContent="cancel";
+      cancelButton.classList.add('cancel');
+      cancelButton.type="button";
+      form.append(input, confirmButton, cancelButton);
+      li.append(form);
       projectsList.append(li);
-      
-      
-      
     })
     return {
 
