9864cc337ac3163ed2abe58376069e98a68b192c
Kajetan <kajetan.loska@op.pl>
2022-03-31 14:00:00 +0200

    .

--- src/DOM.js
+++ src/DOM.js
@@ -2,18 +2,6 @@ import Project from "./projects";
 const UI = (() => {
     const createProjectButton = document.getElementById('addProjectButton');
     const projectsList = document.getElementById('projects');
-    // const createForm=()=>{
-    //   const li = document.createElement('li');
-    //   const form = document.createElement('form');
-    //   const input = document.createElement('input');
-    //   input.type='text';
-    //   input.placeholder='Name';
-    //   const confirmButton = document.createElement('button');
-    //   const cancelButton = document.createElement('button');
-    //   form.append(input, confirmButton, cancelButton);
-    //   li.append(form);
-    //   projectsList.append(li);
-    // }
     createProjectButton.addEventListener('click',()=>{
       const li = document.createElement('li');
       const form = document.createElement('form');
@@ -26,12 +14,6 @@ const UI = (() => {
       confirmButton.addEventListener('click',(e)=>{
         e.preventDefault();
         console.log(e);
-        
-        
-        
-        
-        
-        
       })
       const cancelButton = document.createElement('button');
       cancelButton.textContent="cancel";
