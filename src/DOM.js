a8ed30d873cf3129c3704f14cfeed1412d819cad
Kajetan <kajetan.loska@op.pl>
2022-04-10 16:57:01 +0200

    .

--- src/DOM.js
+++ src/DOM.js
@@ -16,6 +16,7 @@ const UI = (() => {
         console.log(e);
   
         
+        
       })
       const cancelButton = document.createElement('button');
       cancelButton.textContent="cancel";
